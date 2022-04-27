08f881a69e1332a74c3b94b7c0f5c65b7d90ef58
Oskar Aidantausta <oskai754@student.liu.se>
2022-04-27 19:20:53 +0200

    added functionality to open all bands or only rgb bands
    modified class method open_h5

--- dataset/datasets.py
+++ dataset/datasets.py
@@ -9,25 +9,31 @@ from torch.utils.data import Dataset
 class sentinel(Dataset):
     
     @staticmethod 
-    def open_h5(path):
+    def open_h5(path,rgb = False):
         with h5py.File(path, 'r') as h5:
             
-            y = torch.from_numpy(h5['train_id'][:,:].astype('long'))
-            x = np.moveaxis(h5['raw'][:,:,:],0,-1)
-            return(x,y)
-        
-    def __init__(self, img_transform=None,label_transform=None,padding=None,root_dir=None, ext='*.nc'):
+            labl = torch.from_numpy(h5['train_id'][:,:].astype('long'))
+            if rgb:
+                #img = torch.from_numpy(h5['raw'][0:3,:,:])
+                img = np.moveaxis(h5['raw'][:,:,:],0,-1)
+            else:
+                #img =torch.from_numpy(h5['raw'][:,:,:])
+                img = np.moveaxis(h5['raw'][:,:,:],0,-1)
+            return(img,labl)
+    
+    def __init__(self, img_transform=None,label_transform=None,padding=None,root_dir=None, ext='*.nc',rgb=False):
         self.root_dir = root_dir #dataset dir
         self.patch_files = glob.glob(os.path.join(self.root_dir, ext))
         self.padding = padding
         self.img_transform = img_transform
         self.label_transform=label_transform 
+        self.rgb = rgb
         
     def __len__(self):
         return len(self.patch_files)
 
     def __getitem__(self, idx):
-        x,y = self.open_h5(self.patch_files[idx])
+        x,y = self.open_h5(self.patch_files[idx], self.rgb)
         
         if self.img_transform:
             x = self.img_transform(x)

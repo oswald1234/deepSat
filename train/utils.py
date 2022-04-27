a771ed5e68bd2a413df46e0143151d7337cb8883
Oskar Aidantausta <oskai754@student.liu.se>
2022-04-27 19:43:57 +0200

    update utils.py
    removed unused function train_one_epoch

--- train/utils.py
+++ train/utils.py
@@ -3,37 +3,6 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 import math
 
-def train_one_epoch(epoch_index,tb_writer):
-    running_loss = 0.
-    last_loss=0.
-
-    for i, data in tqdm(enumerate(training_loader),desc='Batch'):
-        # Every data instance is an input (X) + label (y) pair
-        inputs,labels = data
-        
-        # zero gradients for every batch
-        optimizer.zero_grad()
-        # make predictions for batch
-        output = model(inputs)
-        
-        # compute loss and gradients   
-        loss = loss_fn(output,labels)
-        loss.backward()
-        
-        # Adjust learning weights
-        optimizer.step()
-            
-        #Gather data and report
-        running_loss += loss.item()
-        if i % 10==9:
-            last_loss = running_loss / 10 # avg per batch loss for last 10 batches
-            print(' batch {} loss: {}'.format(i+1,last_loss))
-            # to tensor board
-            tb_x = epoch_index * len(training_loader) + i + 1
-            tb_writer.add_scalar('Loss/train',last_loss,tb_x)
-            running_loss = 0.
-                
-    return last_loss
 
 # NB! When calling on confMat() and metric functions, save all individual class
 # confusion matrices and metrics. The data will be used as input arguments when

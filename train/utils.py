fdda0ae222b7fbd068b3e1b133b8772788746371
Paaatrick123 <patas987@student.liu.se>
2022-05-05 15:37:11 +0200

    Update utils.py
    Added function to compute weights wrt entire dataset and samples

--- train/utils.py
+++ train/utils.py
@@ -2,7 +2,7 @@ from tqdm.notebook import tqdm
 from sklearn.metrics import confusion_matrix
 import numpy as np
 import math
-
+import torch
 
 # NB! When calling on confMat() and metric functions, save all individual class
 # confusion matrices and metrics. The data will be used as input arguments when
@@ -90,3 +90,54 @@ def minMaxPercentile(x,per=2):
 # maxPer = Max percentile (value) 
 def normalizeData(x,minPer,maxPer):
     return (x-minPer)/(maxPer-minPer)
+
+# trainiter = iter(DataLoader(dataset))
+# c         = 27 (number of classes including unclassified class)
+def weights(trainiter,c=27):
+    # Count all classes/labels in the enitre dataset and per sample
+    # Compute weights wrt the entire dataset and per sample
+    
+    n_classes_dataset = torch.zeros(c,dtype=torch.int)
+    class_weights_dataset = torch.zeros(c,dtype=torch.float)
+    
+    n_classes_sample = torch.zeros(len(trainiter),c,dtype=torch.int)
+    class_weights_sample = torch.zeros(len(trainiter),c,dtype=torch.float)
+    
+    # Count classes/labels in entire dataset. Save total count and count per sample
+    # i = samples/patches in dataset, j=examples/instances/rows in sample/patch
+    for i, (_, labels) in enumerate(trainiter):
+        classes,count = labels.unique(return_counts=True)
+        for j in classes:
+            idx = (classes==j).nonzero().item()
+            classCount = count[idx].item()
+            n_classes_dataset[j] += classCount
+            n_classes_sample[i][j] = classCount
+    
+    ############### Weights wrt sample
+    
+    # Set class weight = 0 for label = 0
+    # Inf control. Init torch.zero not actually "zero"
+    for i in range(len(n_classes_sample)):
+        for j in range(len(n_classes_sample[i])):
+            if j == 0:
+                class_weights_sample[i][j] = 0
+            elif n_classes_sample[i][j].item() != 0:
+                class_weights_sample[i][j] = torch.max(n_classes_sample[i])/n_classes_sample[i][j]
+                
+    ################ Weights wrt dataset
+    
+    #class_weights_dataset=n_classes_dataset/n_classes_dataset.sum()
+    
+    # Set class weight = 0 for label = 0
+    #class_weights_dataset[0] = 0
+    
+    # Inf control. Init torch.zero not actually "zero"
+    for i, count in enumerate(n_classes_dataset):
+        if i == 0:
+            class_weights_dataset[i] = 0
+        elif count.item() != 0:
+            class_weights_dataset[i] = torch.max(n_classes_dataset)/n_classes_dataset[i]
+    
+    return class_weights_dataset, class_weights_sample
+            
+    ################

c8867955e21c2e925813e2fb9e61fbf08a0d8d0d
Paaatrick123 <patas987@student.liu.se>
2022-05-11 16:38:09 +0200

    Code cleanup
    weights() is now splitted into classCount() and crossEntropyLossWeights. classCount() is needed for other functions and defining crossEntropyLossWeights makes its use clearer.

--- dataset/utils.py
+++ dataset/utils.py
@@ -1,20 +1,18 @@
-
-
+import torch
 
 # trainiter = iter(DataLoader(dataset))
 # c         = 27 (number of classes including unclassified class)
-def weights(trainiter,c=27):
-    # Count all classes/labels in the enitre dataset and per sample
-    # Compute weights wrt the entire dataset and per sample
+
+# Returns class count wrt dataset: n_classes_dataset, tensor(n_classes)
+#                     wrt sample:  n_classes_sample,  tensor(n_samples, n_classes)
+def classCount(trainiter,c=27):
     
     n_classes_dataset = torch.zeros(c,dtype=torch.int)
-    class_weights_dataset = torch.zeros(c,dtype=torch.float)
     
     n_classes_sample = torch.zeros(len(trainiter),c,dtype=torch.int)
-    class_weights_sample = torch.zeros(len(trainiter),c,dtype=torch.float)
     
-    # Count classes/labels in entire dataset. Save total count and count per sample
-    # i = samples/patches in dataset, j=examples/instances/rows in sample/patch
+    # i = samples/patches in dataset, j = class in sample/patch
+    # j = 0 => label 0, j = 1 => label 1.... j = 26 => label 26 
     for i, (_, labels) in enumerate(trainiter):
         classes,count = labels.unique(return_counts=True)
         for j in classes:
@@ -22,11 +20,30 @@ def weights(trainiter,c=27):
             classCount = count[idx].item()
             n_classes_dataset[j] += classCount
             n_classes_sample[i][j] = classCount
+            
+    return n_classes_dataset, n_classes_sample
+
+# trainiter = iter(DataLoader(dataset))
+# c         = 27 (number of classes including unclassified class)
+
+# Returns cross entropy loss weights wrt dataset: n_classes_dataset, tensor(n_classes)
+#                                    wrt sample:  n_classes_sample,  tensor(n_samples, n_classes)
+
+def crossEntropyLossWeights(trainiter,c=27):
+    
+    n_classes_dataset = torch.zeros(c,dtype=torch.int)
+    n_classes_sample = torch.zeros(len(trainiter),c,dtype=torch.int)
+    n_classes_dataset, n_classes_sample = classCount(trainiter,c)
+    
+    class_weights_dataset = torch.zeros(c,dtype=torch.float)
+    class_weights_sample = torch.zeros(len(trainiter),c,dtype=torch.float)
     
     ############### Weights wrt sample
     
     # Set class weight = 0 for label = 0
     # Inf control. Init torch.zero not actually "zero"
+    # i = samples/patches in dataset, j = class in sample/patch
+    # j = 0 => label 0, j = 1 => label 1.... j = 26 => label 26 
     for i in range(len(n_classes_sample)):
         for j in range(len(n_classes_sample[i])):
             if j == 0:
@@ -34,10 +51,12 @@ def weights(trainiter,c=27):
             elif n_classes_sample[i][j].item() != 0:
                 class_weights_sample[i][j] = torch.max(n_classes_sample[i])/n_classes_sample[i][j]
 
-                ################ Weights wrt dataset
+    ################ Weights wrt dataset
                 
     # Set class weight = 0 for label = 0
     # Inf control. Init torch.zero not actually "zero"
+    # i = class in sample/patch
+    # i = 0 => label 0, i = 1 => label 1.... i = 26 => label 26 
     for i, count in enumerate(n_classes_dataset):
         if i == 0:
             class_weights_dataset[i] = 0
@@ -45,8 +64,7 @@ def weights(trainiter,c=27):
             class_weights_dataset[i] = torch.max(n_classes_dataset)/n_classes_dataset[i]
     
     return class_weights_dataset, class_weights_sample
-
-
+    
 ########### Transformations ############# 
 
     # Global percentile Min-Max normalization, better known as RobustScaler

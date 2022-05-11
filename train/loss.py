28083ae6fb17d628ef5d8bb634fdeb1293b7f373
Paaatrick123 <patas987@student.liu.se>
2022-05-11 11:53:56 +0200

    Code fix
    Assumed 4 indices instead of 3 for labels, fixed code accordingly

--- train/loss.py
+++ train/loss.py
@@ -3,26 +3,26 @@ import torch.nn as nn
 
 ### TVERSKY INDEX (MULTICLASS)
 
-# inputs  = y_pred, tensor(B,C,H,W), B = Batch Size, C = Classes, H = Height, W = Width
-# targets = y_true, tensor(B,C,H,W), B = Batch Size, C = Classes, H = Height, W = Width
+# inputs  = model output, tensor(B,C,H,W), B = Batch Size, C = Classes, H = Height, W = Width
+# targets = labels, tensor(B,H,W), B = Batch Size, H = Height, W = Width
 # alpha   = Scalar (>= 0)
 # beta    = Scalar (>= 0)
 # smooth  = Smoothing factor, scalar (have seen 1e-6 and 1 used as values)
 
-# alpha = beta = 1   = Tanimoto coefficient
-# alpha = beta = 0.5 = Sørensen–Dice coefficient
+# alfa = beta = 1   = Tanimoto coefficient
+# alfa = beta = 0.5 = Sørensen–Dice coefficient
 
 def tverskyIndex(inputs, targets, smooth=1, alpha=0.7, beta=0.3):
     
     NUM_CLASSES = inputs.size(dim=1)
 
-    # Permute BxCxHxW into CxBxHxW, to have classes as the frist index
+    # Permute inputs BxCxHxW into CxBxHxW, to have classes as the frist index
     inputs = inputs.permute(1,0,2,3)
-    targets = targets.permute(1,0,2,3)
     
-    # Then, flatten all other dimensions BxCxHxW --> CxBxHxW --> CxB*H*W
+    # Then, flatten all other input dimensions CxBxHxW --> CxB*H*W
+    # and target dimensions BxHxW --> B*H*W
     inputs = inputs.reshape(NUM_CLASSES, -1)
-    targets = targets.reshape(NUM_CLASSES, -1)
+    targets = targets.reshape(-1)
     
     # https://www.kaggle.com/code/bigironsphere/loss-function-library-keras-pytorch/notebook#Tversky-Loss
     # https://en.wikipedia.org/wiki/Tversky_index
@@ -47,8 +47,8 @@ def tverskyIndex(inputs, targets, smooth=1, alpha=0.7, beta=0.3):
 # Although the best parameter values will take some trial and error to determine, 
 # you should see good results with the following: alpha = 0.7, beta = 0.3, gamma = 3/4
 
-# inputs  = y_pred, tensor(B,C,H,W), B = Batch Size, C = Classes, H = Height, W = Width
-# targets = y_true, tensor(B,C,H,W), B = Batch Size, C = Classes, H = Height, W = Width
+# inputs  = model output, tensor(B,C,H,W), B = Batch Size, C = Classes, H = Height, W = Width
+# targets = labels, tensor(B,H,W), B = Batch Size, H = Height, W = Width
 # alpha   = Scalar (>= 0)
 # beta    = Scalar (>= 0)
 # gamma   = Scalar [1,3]
@@ -60,7 +60,7 @@ class focalTverskyLoss(nn.Module):
 
     def forward(self, inputs, targets, smooth=1, alpha=0.7, beta=0.3, gamma=3/4):
         
-        # Comment out if the model contains a softmax activation layer
+        ##### Comment out if the model contains a softmax activation layer!!!!!! #####
         inputs = torch.softmax(inputs, dim=1)
         
         # .sum() as in Cross Entropy Loss

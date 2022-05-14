d075db7338a5e143d447eae8ddb65936c1ea22d4
Paaatrick123 <patas987@student.liu.se>
2022-05-14 14:38:34 +0200

    Updated metric.py
    castToFloat32 implemented to avoid data overflow, label 0 (unclassified) is now not included in the computation of metrics, wma() now requires class counts instead of dataloader and UA2018 ID is now added to prints.

--- train/metrics.py
+++ train/metrics.py
@@ -1,6 +1,5 @@
 
 from sklearn.metrics import multilabel_confusion_matrix
-from dataset.utils import classCount
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,8 +7,10 @@ import torch
 import math
 import warnings
 
- # NB! See classDict.py!
-strlist = [{}]*27
+# Class descriptions. See classDict.py!
+n_classes_dsc = 27
+ 
+strlist = [{}]*n_classes_dsc 
 strlist[0] = "Unclassified"
 strlist[1] = "Continuous Urban Fabric"
 strlist[2] = "Discontinuous Dense Urban Fabric"
@@ -38,32 +39,76 @@ strlist[24] = "Open spaces with little or no vegetations (beaches, dunes, bare r
 strlist[25] = "Wetland"
 strlist[26] = "Water bodies"
 
-############## HOW TO COMPUTE MODEL METRICS #############
-#    1. computeConfMats()
-#    2. computeMetrics() (use returned metrics in step 3)
-#    3. wma()
-######################################################### 
+# Urban Atlas 2018 IDs
+n_classes_ua = 26
 
-# yTrue = tensor(B,H,W), B = Batch Size, H = Height, W = Width
-# yPred = tensor(B,H,W), B = Batch Size, H = Height, W = Width
+strlist_ua = [{}]*n_classes_ua
+strlist_ua[0] = "11100"
+strlist_ua[1] = "11210"
+strlist_ua[2] = "11220"
+strlist_ua[3] = "11230"
+strlist_ua[4] = "11240"
+strlist_ua[5] = "11300"
+strlist_ua[6] = "12100"
+strlist_ua[7] = "12210/12220"
+strlist_ua[8] = "12230"
+strlist_ua[9] = "12300"
+strlist_ua[10] = "12400"
+strlist_ua[11] = "13100"
+strlist_ua[12] = "13300"
+strlist_ua[13] = "13400"
+strlist_ua[14] = "14100"
+strlist_ua[15] = "14200"
+strlist_ua[16] = "21000"
+strlist_ua[17] = "22000"
+strlist_ua[18] = "23000"
+strlist_ua[19] = "24000"
+strlist_ua[20] = "25000"
+strlist_ua[21] = "31000"
+strlist_ua[22] = "32000"
+strlist_ua[23] = "33000"
+strlist_ua[24] = "40000"
+strlist_ua[25] = "50000"
+
+############################# HOW TO COMPUTE MODEL METRICS ########################
+#    1. computeConfMats()
+#    2. computeMetrics() (use returned class metrics in step 4)
+#    3. classCount()     (in dataset/utils.py, use returned class counts in step 4)
+#    4. wma()
+###################################################################################
+
+# yTrue       = tensor(B,H,W), B = Batch Size, H = Height, W = Width
+# yPred       = tensor(B,H,W), B = Batch Size, H = Height, W = Width
+# n_classes   = number of classes (scalar)
 # Returns ndarray of shape (n_classes, 2, 2) with confusion matrices per class
-def computeConfMats(yTrue,yPred):
-    LABELS = np.arange(27) # (0,1,2...,26)
+def computeConfMats(yTrue,yPred,n_classes=27):
+    LABELS = np.arange(n_classes) # (0,1,2..., n_classes)
     # Flatten dimensions BxHxW --> B*H*W
     yTrue = yTrue.reshape(-1)
     yPred = yPred.reshape(-1)
     cMats = multilabel_confusion_matrix(y_true=yTrue,y_pred=yPred,labels=LABELS)
     return cMats
+    
+# Cast to float32 to avoid data overflow    
+# tn, fp, fn, tp = tensor (scalar)
+def castToFloat32(tn, fp, fn, tp):
+    tn=tn.to(torch.float32)
+    fp=fp.to(torch.float32)
+    fn=fn.to(torch.float32)
+    tp=tp.to(torch.float32)
+    return tn, fp, fn, tp
 
 # Accuracy
 def acc(cMat):
-    tn, fp, fn, tp = cMat.ravel()
-    metric = (tp+tn)/cMat.sum()
-    return metric
+     tn, fp, fn, tp = cMat.ravel()
+     tn, fp, fn, tp = castToFloat32(tn, fp, fn, tp)
+     metric = (tp+tn)/cMat.sum()
+     return metric
        
 # Precision
 def prc(cMat):
     tn, fp, fn, tp = cMat.ravel()
+    tn, fp, fn, tp = castToFloat32(tn, fp, fn, tp)
     if (tp+fp) != 0:
         metric = tp/(tp+fp)
     else:
@@ -74,6 +119,7 @@ def prc(cMat):
 # Recall
 def rcl(cMat):
     tn, fp, fn, tp = cMat.ravel()
+    tn, fp, fn, tp = castToFloat32(tn, fp, fn, tp)
     if (tp+fn) != 0:
         metric = tp/(tp+fn)
     else:
@@ -84,6 +130,7 @@ def rcl(cMat):
 # F1-Score
 def f1s(cMat):
     tn, fp, fn, tp = cMat.ravel()
+    tn, fp, fn, tp = castToFloat32(tn, fp, fn, tp)
     if (fp+tp+tp+fn) != 0:
         metric = (2*tp)/(fp+tp+tp+fn)
     else:
@@ -94,6 +141,7 @@ def f1s(cMat):
 # Intersection over Union
 def iou(cMat):
     tn, fp, fn, tp = cMat.ravel()
+    tn, fp, fn, tp = castToFloat32(tn, fp, fn, tp)
     if (fp+tp+fn) != 0:
         metric = tp/(fp+tp+fn)
     else:
@@ -104,6 +152,7 @@ def iou(cMat):
 # Matthews Correlation Coefficient
 def mcc(cMat):
     tn, fp, fn, tp = cMat.ravel()
+    tn, fp, fn, tp = castToFloat32(tn, fp, fn, tp)
     numerator = tp*tn-fp*fn
     denominator = (tp+fp)*(tp+fn)*(tn+fp)*(tn+fn)
     if denominator != 0:
@@ -113,10 +162,12 @@ def mcc(cMat):
         warnings.warn("MCC: Division by zero. Metric is undefined and set to 0")
     return metric
 
-# cMats = ndarray of shape (n_classes, 2, 2)
+# cMats     = ndarray of shape (n_classes, 2, 2)
+# n_metrics = Number of metrics (scalar)
 # Returns computed metrics per class, ndarray of shape (n_classes, n_metrics)
-def computeMetrics(cMats):
-    metrics=torch.zeros(27,6,dtype=torch.float)
+def computeMetrics(cMats, n_metrics=6):
+    cMats = cMats[1:len(cMats),:,:] #REMOVE LABEL 0 = UNCLASSIFIED
+    metrics=torch.zeros(len(cMats),n_metrics,dtype=torch.float32)
     for i, cMat in enumerate (cMats):
         metrics[i][0] = acc(cMat)
         metrics[i][1] = prc(cMat)
@@ -127,14 +178,17 @@ def computeMetrics(cMats):
     return metrics
 
 # Weighted Macro-Average model metrics
-# metrics per class       = ndarray of shape (n_classes, n_metrics)
-# training_loader         = DataLoader(dataset)
-# c                       = 27 (number of classes including unclassified class)
+# metrics                 = metrics per class, ndarray of shape (n_classes, n_metrics)
+# classCounts             = Class count wrt dataset, tensor(n_classes).
+#                           Call classCount() in dataset/utils.py and
+#                           save the class count
 # Returns weighted macro-averaged model metrics
-def wma(metrics,training_loader,c=27):
-    classCounts,_ = classCount(training_loader,c)
-    metricsWMA = torch.zeros(len(metrics[1]),dtype=torch.float)
-    weights = torch.zeros(len(classCounts),dtype=torch.float)
+def wma(metrics,classCounts):    
+    classCounts = classCounts[1:len(classCounts)] #REMOVE LABEL 0 = UNCLASSIFIED
+    
+    metricsWMA = torch.zeros(len(metrics[1]),dtype=torch.float32)
+    
+    weights = torch.zeros(len(classCounts),dtype=torch.float32)
     weights = classCounts/classCounts.sum()
     
     # Permute metrics (n_classes, n_metrics) --> (n_metrics, n_classes)
@@ -155,11 +209,13 @@ def wma(metrics,training_loader,c=27):
 #                     Call classCount() in dataset/utils.py and
 #                     save the class count
 def valMetric(cMats,classCounts):
+    cMats = cMats[1:len(cMats),:,:] #REMOVE LABEL 0 = UNCLASSIFIED
+    classCounts = classCounts[1:len(classCounts)] #REMOVE LABEL 0 = UNCLASSIFIED
     
-    weights = torch.zeros(len(classCounts),dtype=torch.float)
+    weights = torch.zeros(len(classCounts),dtype=torch.float32)
     weights = classCounts/classCounts.sum()
     
-    iou_class = torch.zeros(len(classCounts),dtype=torch.float)
+    iou_class = torch.zeros(len(classCounts),dtype=torch.float32)
     
     for i, cMat in enumerate (cMats):
         iou_class[i] = iou(cMat)
@@ -173,22 +229,32 @@ def valMetric(cMats,classCounts):
 ############################ Metric Visualization ##########################
 
 # Prints table with metrics per class
-# metrics = = ndarray of shape (n_classes, n_metrics)
-def printClassMetrics(metrics):
+# metrics           = ndarray of shape (n_classes, n_metrics) with class metrics
+# classCounts       = Class count wrt dataset, tensor(n_classes).
+#                     Call classCount() in dataset/utils.py and
+#                     save the class count
+def printClassMetrics(metrics,classCounts):
+        
+    classCounts = classCounts[1:len(classCounts)] #REMOVE LABEL 0 = UNCLASSIFIED
+    classCounts = classCounts/classCounts.sum() * 100    
+    strlist2 = strlist[1:len(strlist)]   #REMOVE LABEL 0 = UNCLASSIFIED
+    
     d = {}
     for i in range(len(metrics)):
-        d[i] = [metrics[i][0].item(), metrics[i][1].item(), metrics[i][2].item(), 
-                metrics[i][3].item(), metrics[i][4].item(), metrics[i][5].item(),strlist[i]]
+        d[i] = [classCounts[i], metrics[i][0].item(), metrics[i][1].item(), metrics[i][2].item(), 
+                metrics[i][3].item(), metrics[i][4].item(), metrics[i][5].item(), strlist2[i], 
+                strlist_ua[i]]
 
     print("@@ Class Metrics @@\n")
 
-    print ("{:<8} {:<15} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(
-        'Label','Accuracy','Precision','Recall','F1-Score','IoU','MCC','Description'))
+    print ("{:<8} {:<15} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10} {:<10}".format(
+        'Label', 'UA2018 ID', '% Data','Accuracy','Precision','Recall',
+        'F1-Score','IoU','MCC','Description'))
 
     for l, v in d.items():
-        acc, prc, rcl, f1s, iou, mcc, dsc = v
-        print ("{:<8} {:<15.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10}".format(
-            l, acc, prc, rcl, f1s, iou, mcc, dsc))
+        p, acc, prc, rcl, f1s, iou, mcc, dsc, ua = v
+        print ("{:<8} {:<15} {:<10.2f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10.4f} {:<10}".format(
+            l+1, ua, p, acc, prc, rcl, f1s, iou, mcc, dsc))
 
 # Prints weighted macro-averaged model metrics 
 # metrics = ndarray with model metrics of shape (n_metrics)
@@ -209,11 +275,14 @@ def printMetrics(metrics):
 # Prints confusion matrices per class        
 # cMats = ndarray of shape (n_classes, 2, 2) with confusion matrices per class
 def printConfusionMatrices(cMats): 
-    labels = ["0","1","2","3","4","5","6","7","8","9","10",
-              "11","12","13","14","15","16","17","18","19",
-              "20","21","22","23","24","25","26"]
+    cMats = cMats[1:len(cMats),:,:]      #REMOVE LABEL 0 = UNCLASSIFIED
+    strlist2 = strlist[1:len(strlist)]   #REMOVE LABEL 0 = UNCLASSIFIED
+     
+    labels = ["1","2","3","4","5","6","7","8","9","10","11",
+              "12","13","14","15","16","17","18","19","20",
+              "21","22","23","24","25","26"]
 
-    fig, axes = plt.subplots(nrows=9, ncols=3, figsize=(20,45))
+    fig, axes = plt.subplots(nrows=13, ncols=2, figsize=(20,45))
 
     group_names = ["True Negative","False Positive","False Negative","True Positive"]
 
@@ -221,14 +290,14 @@ def printConfusionMatrices(cMats):
 
     for row in axes:
         for col in row:
-            group_counts = ["{0:0.0f}".format(value) for value in cMats[counter].flatten()]
-            group_percentages = ["{0:.2%}".format(value) for value in cMats[counter].flatten()/np.sum(cMats[counter])]
+            group_counts = ["{:,}".format(value) for value in cMats[counter].flatten()]
+            group_percentages = ["{0:.2%}".format(value) for value in cMats[counter].flatten()/cMats[counter].sum()]
             labels2 = [f"{v1}\n{v2}\n{v3}" for v1, v2, v3 in zip(group_names,group_counts,group_percentages)]
             labels2 = np.asarray(labels2).reshape(2,2)
 
             sns.set(font_scale=1.1)
             sns.heatmap(cMats[counter], annot=labels2, fmt="", cmap='Blues', ax=col)
-            col.set_title("Label " + labels[counter] + ": " + strlist[counter], fontsize=12)
+            col.set_title("Label " + labels[counter] + " (UA18: " + strlist_ua[counter] + "): " + strlist2[counter], fontsize=11)
             col.set_xlabel("Predicted Label", fontsize=12)
             col.set_ylabel("True Label", fontsize=12)
 
